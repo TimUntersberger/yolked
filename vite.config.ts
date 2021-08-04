1b88574d05d90df8f0e82cae33e0fa27f8d4bd56
TimUntersberger <timuntersberger2@gmail.com>
2021-08-04 20:23:41 +0200

    feat(#3): add manifest.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,26 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import mkcert from "vite-plugin-mkcert"
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import mkcert from "vite-plugin-mkcert";
+import { VitePWA as pwa } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    https: true
+    https: true,
   },
   base: "/yolked/",
-  plugins: [reactRefresh(), mkcert({
-    hosts: ["localhost", "www.localhost.com", "localhost.com"]
-  })]
-})
+  plugins: [
+    reactRefresh(),
+    mkcert({
+      hosts: ["localhost", "www.localhost.com", "localhost.com"],
+    }),
+    pwa({
+      manifest: {
+        name: "Yolked",
+        short_name: "Yolked",
+        start_url: "/yolked",
+        display: "standalone"
+      }
+    })
+  ],
+});
